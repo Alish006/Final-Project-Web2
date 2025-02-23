5cfd6b03f84a89bdd938e1a18048f772cf0924f0
Alish006 <153196767+Alish006@users.noreply.github.com>
2025-02-23 16:11:11 +0500

    habit

--- routes/habit.js
+++ routes/habit.js
@@ -6,25 +6,27 @@ const { authenticateToken } = require('../middleware/authenticationVerify.js');
 
 router.get('/habit', authenticateToken, async (req, res)=>{
     try{
-        const habit = await Habit.find({userId: req.user.id, isAdmin: req.user.Isadmin});
+        const habit = await Habit.find({userId: req.user.userId}).sort({ createdAt: -1 });;
 
-        let habitHTML = ' ';
+        let habitHTML = " ";
 
         habit.forEach(habits => {
             habitHTML += 
             `
+            <style>
+            body{ background-color: #007bff; justify-self: center; }
+            p{font-size: 25px;}
+            button{ background:rgb(200, 0, 255); color: white; border: none; padding: 10px 15px; font-size: 20px; border-radius: 5px; cursor: pointer; }
+            </style> 
+
         <div>
           <p>ID: ${habits.id}</p>
           <p>Name: ${habits.name}</p>
           <p>Description:${habits.description}</p>
-          <p>WeeklyStatus: ${tasks.weeklyStatus}</p>
+          <p>WeeklyStatus: ${habits.weeklyStatus ? 'Yes' : 'No'}</p>
         
           <form action="/habit/post" method="get">
                <button type="submit">Edit</button>
-          </form>
-
-          <form action="/habit/${habits.id}/edit" method="get">
-               <button type="submit">Create</button>
           </form>
 
            ${req.user.isAdmin ? `
@@ -42,8 +44,17 @@ router.get('/habit', authenticateToken, async (req, res)=>{
 
         res.status(200).send(
             `
+            <style>
+            body{ background-color: #007bff; justify-self: center; }
+            h1{ text-align: center; }
+            button { background: rgb(200, 0, 255);; color: white; border: none; padding: 10px 15px; font-size: 20px; border-radius: 5px; cursor: pointer; }
+            </style>
+
             <h1>Habits</h1>
               ${habitHTML}
+            <form action="/habit/post" method="get">
+                  <button type="submit">Create</button>
+            </form>
             `
         );
         
@@ -53,7 +64,44 @@ router.get('/habit', authenticateToken, async (req, res)=>{
 });
 
 router.get('/habit/post', authenticateToken, (req, res) => {
-    res.send(__dirname + '/index.html');
+    res.render('habitPost.ejs');
+});
+
+
+router.post('/habit', authenticateToken, async (req, res) =>{
+    try{
+
+        const habitData = {
+            name: req.body.name,
+            description: req.body.description,
+            weeklyStatus: req.body.weeklyStatus || false,
+            userId: req.user.userId,
+        };
+        
+        const habit = await Habit.create(habitData);
+        
+        res.status(200).send(
+            `
+          <style>
+            body{ background-color: #007bff; justify-self: center; }
+            h1{ text-align: center; }
+            p{font-size: 25px;}
+            button { background: rgb(200, 0, 255);; color: white; border: none; padding: 10px 15px; font-size: 20px; border-radius: 5px; cursor: pointer; }
+          </style>
+          <h1>Habit Created Successfully!</h1>
+          <p>Name: ${habit.name}</p>
+          <p>Description: ${habit.description}</p>
+
+          <form action="/habit" method="get">
+          <button type="submit">Get</button>
+           </form>
+            `
+        )
+
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+
 });
 
 
